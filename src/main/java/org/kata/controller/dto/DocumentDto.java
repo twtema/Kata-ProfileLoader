536406bb912f6feb13ee7766ce2c616d71222e29
kamil <kh8@ramler.ru>
2023-10-29 23:58:21 +0300

    убрал лишний сервис и контроллер, изменил ДТО добавил маппиг в маппер

--- src/main/java/org/kata/controller/dto/DocumentDto.java
+++ src/main/java/org/kata/controller/dto/DocumentDto.java
@@ -26,11 +26,4 @@ public class DocumentDto {
 
     private boolean actual;
 
-//    public void setActual(boolean actual) {
-//        this.actual = actual;
-//    }
-//
-//    public boolean isActual() {
-//        return actual;
-//    }
 }
