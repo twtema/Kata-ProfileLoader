3f29d1c0b931b35843cfce8f71570a19a0f1b8a1
defeatedSanity <rome.kuznets@gmail.com>
2023-11-02 18:48:50 +0300

    add mapper for wallet

---
+++ src/main/java/org/kata/service/mapper/WalletMapper.java
@@ -0,0 +1,8 @@
+package org.kata.service.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface WalletMapper {
+}
