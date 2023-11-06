eed428aeab0099951f257fdbf8ecccd1e75be0a6
Maksim Malykhin <131053398+MaksimArs@users.noreply.github.com>
2023-11-06 11:14:19 +0300

    [Task-1] Описал методы контроллера Swagger аннотациями для МКС ProfileLoader (#1)
    * Описал методы контроллера Swagger аннотациями для МКС ProfileLoader
    
    * Описал методы контроллера Swagger аннотациями для МКС ProfileLoader
    
    * Описал методы контроллера Swagger аннотациями для МКС ProfileLoader
    
    * Добавил example

--- src/main/java/org/kata/controller/ContactMediumController.java
+++ src/main/java/org/kata/controller/ContactMediumController.java
@@ -1,5 +1,9 @@
 package org.kata.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.kata.controller.dto.ContactMediumDto;
 import org.kata.exception.ContactMediumNotFoundException;
@@ -18,13 +22,22 @@ public class ContactMediumController {
 
     private final ContactMediumService contactMediumService;
 
+    @Operation(summary = "Получить ContactMedium по icp",
+            description= "Возвращает DTO ContactMedium по ICP")
     @GetMapping
-    public ResponseEntity<List<ContactMediumDto>> getContactMedium(@RequestParam String icp) {
+    public ResponseEntity<List<ContactMediumDto>> getContactMedium(
+            @Parameter(description = "ICP ContactMedium") @RequestParam String icp) {
         return new ResponseEntity<>(contactMediumService.getContactMedium(icp), HttpStatus.OK);
     }
 
+    @Operation(summary = "Создать новый ContactMedium", description = "Сохраняет и возвращает DTO нового контакта")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "ContactMedium успешно создан"),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос")
+    })
     @PostMapping
-    public ResponseEntity<ContactMediumDto> postContactMedium(@RequestBody ContactMediumDto dto) {
+    public ResponseEntity<ContactMediumDto> postContactMedium(
+            @Parameter(description = "DTO ContactMedium для создания") @RequestBody ContactMediumDto dto) {
         return new ResponseEntity<>(contactMediumService.saveContactMedium(dto), HttpStatus.CREATED);
     }
 
--- src/main/java/org/kata/controller/dto/AddressDto.java
+++ src/main/java/org/kata/controller/dto/AddressDto.java
@@ -1,5 +1,6 @@
 package org.kata.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -7,18 +8,25 @@ import lombok.extern.jackson.Jacksonized;
 @Data
 @Builder
 @Jacksonized
+@Schema(description = "DTO представляющий Адрес")
 public class AddressDto {
 
+    @Schema(description = "ICP", example = "1234567890")
     private String icp;
 
+    @Schema(description = "Улица", example = "Пушкина")
     private String street;
 
+    @Schema(description = "Город", example = "Москва")
     private String city;
 
+    @Schema(description = "Государство", example = "Российская Федерация")
     private String state;
 
+    @Schema(description = "Почтовый индекс", example = "101000")
     private String postCode;
 
+    @Schema(description = "Страна", example = "Россия")
     private String country;
 
 }
