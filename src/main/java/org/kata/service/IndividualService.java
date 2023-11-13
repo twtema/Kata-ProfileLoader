f35905d6a87bec0d06a9015c9120cc6c813ac24f
MaksimArs <maxim.ars_1@mail.ru>
2023-11-14 00:23:45 +0300

    [Task-20] (ProfileLoader) Поиск Individual по его ContactMedium.phone в ProfileService

--- src/main/java/org/kata/service/IndividualService.java
+++ src/main/java/org/kata/service/IndividualService.java
@@ -4,6 +4,7 @@ import org.kata.controller.dto.IndividualDto;
 
 public interface IndividualService {
     IndividualDto getIndividual(String icp);
+    IndividualDto getIndividualByPhone(String phone);
 
     IndividualDto saveIndividual(IndividualDto dto);
 }
