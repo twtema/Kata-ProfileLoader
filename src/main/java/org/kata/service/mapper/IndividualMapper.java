6d843025adf59b9899e15c799c4b7b3ac8155214
masamune <emopanda1337@gmail.com>
2024-01-07 18:57:01 +0700

    hex generation for test individuals avatars

--- src/main/java/org/kata/service/mapper/IndividualMapper.java
+++ src/main/java/org/kata/service/mapper/IndividualMapper.java
@@ -1,9 +1,16 @@
 package org.kata.service.mapper;
 
+import org.kata.controller.dto.AvatarDto;
 import org.kata.controller.dto.IndividualDto;
+import org.kata.entity.Avatar;
 import org.kata.entity.Individual;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Formatter;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface IndividualMapper {
@@ -12,4 +19,37 @@ public interface IndividualMapper {
 
     IndividualDto toDto(Individual individual);
 
+    default Avatar avatarDtoToAvatar(AvatarDto avatarDto) {
+        if (avatarDto == null) {
+            return null;
+        } else {
+            Avatar avatar = new Avatar();
+            avatar.setFilename(avatarDto.getFilename());
+            byte[] imageData = avatarDto.getImageData();
+            if (imageData != null) {
+                avatar.setImageData(Arrays.copyOf(imageData, imageData.length));
+            }
+            avatar.setHex(SHAsum(imageData));
+            return avatar;
+        }
+    }
+
+    private String SHAsum(byte[] convertme) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return byteArray2Hex(md.digest(convertme));
+    }
+
+    private String byteArray2Hex(final byte[] hash) {
+        try (Formatter formatter = new Formatter()) {
+            for (byte b : hash) {
+                formatter.format("%02x", b);
+            }
+            return formatter.toString();
+        }
+    }
 }
