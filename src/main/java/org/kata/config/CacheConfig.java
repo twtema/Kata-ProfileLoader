39b4d6de2ab9081e2f7d9b9fe6908f0eb0ad2548
Artiom <double99time@gmail.com>
2024-02-25 14:41:26 +0300

    Commit 001 - change name of table from saving_account to savings_account in 18_change_set_add_table_savings_account.xml and in SavingsAccount.java - change name of column from info_Of_Percent to info_of_percent in 18_change_set_add_table_savings_account.xml and in SavingsAccount.java

--- src/main/java/org/kata/config/CacheConfig.java
+++ src/main/java/org/kata/config/CacheConfig.java
@@ -1,31 +1,24 @@
 package org.kata.config;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.util.Converter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheResolver;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleCacheResolver;
-import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.*;
-import redis.clients.jedis.Jedis;
+import org.springframework.data.redis.core.convert.RedisCustomConversions;
 
-import java.lang.reflect.Method;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableCaching
@@ -52,106 +45,25 @@ public class CacheConfig {
 //    }
 
     @Bean
+    public RedisCustomConversions redisCustomConversions() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        List<Converter<?, ?>> converters = new ArrayList<>();
+        converters.add(new AuthenticationSessionConnverter(objectMapper).getConverters);
+
+        return new RedisCustomConversions(converters);
+    }
+
+
+//        new AuthenticationSessionConnverter(objectMapper).getConverters
+
+    @Bean
     public CacheResolver setCacheResolver(CacheManager cacheManager) {
         return new SimpleCacheResolver(cacheManager);
     }
 
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofHours(3))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new SetRedisSerializer()));
-
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(cacheConfiguration)
-                .build();
-    }
-
-    @Bean
-    public Jedis jedis() {
-        return new Jedis(host, port);
-    }
-
-    @Bean
-    public CacheResolver cacheResolver(CacheManager cacheManager) {
-        return new SimpleCacheResolver(cacheManager);
-    }
-
-//    @Override
-//    public KeyGenerator keyGenerator() {
-//        return new SimpleKeyGenerator() {
-//            @Override
-//            public Object generate(Object target, Method method, Object... params) {
-//                StringBuilder keyBuilder = new StringBuilder();
-//                keyBuilder.append(method.getName());
-//                for (Object param : params) {
-//                    keyBuilder.append("_").append(param.toString());
-//                }
-//                return keyBuilder.toString();
-//            }
-//        };
-//    }
-
-//    private static class SetRedisSerializer implements RedisSerializer<Set<?>> {
-//
-////        private final RedisSerializer<String> stringSerializer = RedisSerializer.string();
-//        private final RedisSerializer<String> stringSerializer = new StringRedisSerializer();
-//
-//
-//        @Override
-//        public byte[] serialize(Set<?> set) throws SerializationException {
-//            // Преобразуйте Set в массив байтов
-//            // Например, можно использовать JSON сериализацию
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            try {
-//                return objectMapper.writeValueAsBytes(set);
-//            } catch (JsonProcessingException e) {
-//                throw new SerializationException("Error serializing set", e);
-//            }
-//        }
-//
-//        @Override
-//        public Set<?> deserialize(byte[] bytes) throws SerializationException {
-//            // Преобразуйте массив байтов обратно в Set
-//            // Например, можно использовать JSON десериализацию
-//            String setString = stringSerializer.deserialize(bytes);
-//            // В данном примере, предполагается, что Set был сериализован в виде строки
-//            // и будет десериализован обратно в Set
-//            return new HashSet<>(Arrays.asList(setString.split(",")));
-//        }
-//    }
-
-
-//    @Bean
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-//        return redisTemplate;
-//    }
-
-
-//    MyObject myObject = new MyObject();
-//        myObject.setName("Test");
-//        myObject.setValue(123);
-//
-//    // Сериализация объекта в JSON
-//    Gson gson = new Gson();
-//    String json = gson.toJson(myObject);
-//
-//    // Сохранение сериализованного объекта в Redis
-//        try (Jedis jedis = new Jedis("localhost")) {
-//        jedis.set("myObjectKey", json);
-//    }
-//
-//    // Десериализация объекта из Redis
-//    String retrievedJson = jedis.get("myObjectKey");
-//    MyObject retrievedObject = gson.fromJson(retrievedJson, MyObject.class);
-
-
-//    @Bean
-//    public RedisTemplate<Object, Object> redisTemplate(){
-//        return  new RedisTemplate<Object, Object>();
-//    }
 }
