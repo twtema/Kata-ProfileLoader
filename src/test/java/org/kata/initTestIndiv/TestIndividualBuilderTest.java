9f42203bc4970d61ed33b26b039075555b9fbc53
Bogdan <bog.bulgakov@yandex.ru>
2024-03-06 00:07:01 +0300

    task 60t add tests for individualInitializer()

--- src/test/java/org/kata/initTestIndiv/TestIndividualBuilderTest.java
+++ src/test/java/org/kata/initTestIndiv/TestIndividualBuilderTest.java
@@ -2,14 +2,61 @@ package org.kata.initTestIndiv;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.kata.entity.Document;
 import org.kata.entity.Individual;
 
-//Тест на создание индивидуала с заполненными полями
 
 class TestIndividualBuilderTest {
 
+    //Тест на проверку пустоты объектов связанных с индувидиулом
     @Test
-    void individualInitializer() {
+    void testIndividualInitializerReturnsNonEmptyLists() {
+        TestIndividual testIndividual = new TestIndividual();
+        TestAddress testAddress = new TestAddress();
+        TestAvatar testAvatar = new TestAvatar();
+        TestContacts testContacts = new TestContacts();
+        TestDocument testDocument = new TestDocument();
+        TestWallet testWallet = new TestWallet();
+        TestSavingsAccount testSavingsAccount = new TestSavingsAccount();
+
+        Assertions.assertNotNull(testIndividual);
+        Assertions.assertNotNull(testAddress);
+        Assertions.assertNotNull(testAvatar);
+        Assertions.assertNotNull(testContacts);
+        Assertions.assertNotNull(testDocument);
+        Assertions.assertNotNull(testWallet);
+        Assertions.assertNotNull(testSavingsAccount);
+
+    }
+
+    //Тест на то, что метод individualInitializer() возвращает Individual
+    @Test
+    void testRelationshipBetweenIndividualAndOtherObjects() {
+        Individual individual = new TestIndividualBuilder().individualInitializer();
+        Assertions.assertNotNull(individual);
+        Assertions.assertInstanceOf(Individual.class, individual);
+    }
+
+    //Тест на создание индивидуала с заполненными полями при создании объекта
+    @Test
+    void builderTestIndividual() {
+        TestIndividual testIndividual = new TestIndividual();
+
+        Assertions.assertFalse(testIndividual.getIcp().isEmpty());
+        Assertions.assertFalse(testIndividual.getName().isEmpty());
+        Assertions.assertFalse(testIndividual.getSurname().isEmpty());
+        Assertions.assertFalse(testIndividual.getFullName().isEmpty());
+        Assertions.assertFalse(testIndividual.getCountryOfBirth().isEmpty());
+        Assertions.assertFalse(testIndividual.getGender().toString().isEmpty());
+        Assertions.assertFalse(testIndividual.getPatronymic().isEmpty());
+        Assertions.assertFalse(testIndividual.getPlaceOfBirth().isEmpty());
+        Assertions.assertFalse(testIndividual.getUuid().isEmpty());
+        Assertions.assertFalse(testIndividual.getBirthDate().toString().isEmpty());
+    }
+
+    //Тест на создание связывание индивидуала с остальными объектами
+    @Test
+    void individualInitializerNotIsEmpti() {
 
         Individual individual = new TestIndividualBuilder().individualInitializer();
 
@@ -31,4 +78,5 @@ class TestIndividualBuilderTest {
         Assertions.assertFalse(individual.getAddress().isEmpty());
 
     }
+
 }
