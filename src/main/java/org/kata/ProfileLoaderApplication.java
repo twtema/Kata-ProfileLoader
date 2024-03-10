b9e082cc98a4b4d71e3c619c3c8796528472c9a0
Bogdan <bog.bulgakov@yandex.ru>
2024-03-10 15:25:42 +0300

    Task 63 completed spring boot 2.6.3 spring cloud 2021.0.3 kafka 2.8.0

--- src/main/java/org/kata/ProfileLoaderApplication.java
+++ src/main/java/org/kata/ProfileLoaderApplication.java
@@ -3,9 +3,11 @@ package org.kata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCaching
 @SpringBootApplication
+@EnableEurekaClient
 public class ProfileLoaderApplication {
 
     public static void main(String[] args) {
