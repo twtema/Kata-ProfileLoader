f520aa8563d0304cc336d5c38e05308079481689
Artiom <double99time@gmail.com>
2024-03-10 19:36:33 +0300

    commit 005

--- src/main/java/org/kata/service/impl/FraudstersDetectionServiceImpl.java
+++ src/main/java/org/kata/service/impl/FraudstersDetectionServiceImpl.java
@@ -1,10 +1,13 @@
 package org.kata.service.impl;
 
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kata.controller.dto.ContactMediumDto;
 import org.kata.controller.dto.IndividualDto;
 import org.kata.entity.blackList.BlackListContacts;
+import org.kata.entity.blackList.BlackListDocuments;
+import org.kata.entity.blackList.BlackListIndividualBirthDate;
 import org.kata.exception.IntrudersDetectionException;
 import org.kata.service.FraudstersDetectionService;
 import org.springframework.stereotype.Service;
@@ -13,12 +16,14 @@ import java.util.List;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class FraudstersDetectionServiceImpl implements FraudstersDetectionService {
+
+    private final BlackListContacts blackListContacts;
 
 
     private boolean isInvalidPhoneNumber(List<ContactMediumDto> listDto) {
-        BlackListContacts contacts = new BlackListContacts();
-        return listDto.stream().anyMatch(contactDto -> contacts.getNumbervalue().stream()
+        return listDto.stream().anyMatch(contactDto -> blackListContacts.getNumbervalue().stream()
                         .anyMatch(contactBlackList -> contactDto.getValue().equals(contactBlackList)));
     }
 
